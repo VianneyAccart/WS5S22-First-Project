0f5128ba96d4fa49478291b3644f63ee6e93f1b7
Vianney Accart <vianneyaccart@gmail.com>
2022-09-15 15:14:21 +0200

    Fetch wilders from API

--- web-app/src/pages/Home/Home.js
+++ web-app/src/pages/Home/Home.js
@@ -1,68 +1,37 @@
 import styles from "./Home.module.scss";
 import Wilder from "../../components/Wilder/Wilder";
-
-const WILDERS = [
-  {
-    id: "wilder-1",
-    firstname: "Vianney",
-    lastname: "Accart",
-    skills: [
-      {
-        id: "skill-1",
-        skillName: "PHP",
-      },
-    ],
-  },
-  {
-    id: "wilder-2",
-    firstname: "Adam",
-    lastname: "Roux",
-    skills: [
-      {
-        id: "skill-2",
-        skillName: "JavaScript",
-      },
-    ],
-  },
-  {
-    id: "wilder-3",
-    firstname: "Ario",
-    lastname: "Ngu",
-    skills: [
-      {
-        id: "skill-1",
-        skillName: "PHP",
-      },
-      {
-        id: "skill-2",
-        skillName: "JavaScript",
-      },
-      {
-        id: "skill-3",
-        skillName: "TypeScript",
-      },
-      {
-        id: "skill-4",
-        skillName: "Java",
-      },
-    ],
-  },
-];
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [wilders, setWilders] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("/wilders");
+      const wilders = await response.json();
+      setWilders(wilders);
+      setIsLoading(false);
+    })();
+  }, []);
+
   return (
     <>
       <h2>Wilders</h2>
-      <section className={styles.cardRow}>
-        {WILDERS.map((wilder) => (
-          <Wilder
-            key={wilder.id}
-            firstname={wilder.firstname}
-            lastname={wilder.lastname}
-            skills={wilder.skills}
-          />
-        ))}
-      </section>
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <section className={styles.cardRow}>
+          {wilders?.map((wilder) => (
+            <Wilder
+              key={wilder.id}
+              firstname={wilder.firstname}
+              lastname={wilder.lastname}
+              skills={wilder.skills}
+            />
+          ))}
+        </section>
+      )}
     </>
   );
 };
