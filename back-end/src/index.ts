8dab7e1a674f499265a2124f159299f1f393696c
Vianney Accart <vianneyaccart@gmail.com>
2022-10-18 11:18:32 +0200

    Replace Express server by dummy Apollo server

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -4,6 +4,8 @@ import schoolsController from "./controllers/school.controller";
 import WilderRepository from "./models/Wilder/wilder.repository";
 import SkillRepository from "./models/Skill/skill.repository";
 import SchoolRepository from "./models/School/school.repository";
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 
 const app = express();
 app.use(express.json());
@@ -19,9 +21,64 @@ app.post(`${WILDERS_PATH}/:id/skills`, wildersController.addSkill);
 const SCHOOL_PATH = "/schools";
 app.get(SCHOOL_PATH, schoolsController.get);
 
-const PORT = 4000;
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-async function start() {
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: "bounded",
+  /**
+   * What's up with this embed: true option?
+   * These are our recommended settings for using AS;
+   * they aren't the defaults in AS3 for backwards-compatibility reasons but
+   * will be the defaults in AS4. For production environments, use
+   * ApolloServerPluginLandingPageProductionDefault instead.
+   **/
+  plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+});
+
+// The `listen` method launches a web server.
+server.listen().then(async ({ url }) => {
   await SkillRepository.initializeRepository();
   await SchoolRepository.initializeRepository();
   await WilderRepository.initializeRepository();
@@ -29,9 +86,6 @@ async function start() {
   await SkillRepository.initializeSkills();
   await SchoolRepository.initializeSchools();
   await WilderRepository.initializeWilders();
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT} !`);
-  });
-}
 
-start();
+  console.log(`🚀  Server ready at ${url}`);
+});
